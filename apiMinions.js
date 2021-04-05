05570a0fcb400bdc5b1e5fe79e4da58e051afb9e
Austin <austin.kraft@gmail.com>
2021-04-05 12:28:36 -0700

    Original updates with Brian

---
+++ apiMinions.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const { ModuleFilenameHelpers } = require('webpack');
+
+const minionsRouter = express.Router;
+//require any relevate ____ from database 
+const 
+
+const {getAllFromDatabase, getFromDatabaseById, 
+    addToDatabase, updateInstanceInDatabase, 
+    deleteFromDatabasebyId, deleteAllFromDatabase} = require('./db');
+
+    const app = require('./server');
+
+
+
+minionsRouter.get('./minions', (req, res, next) => {
+    const allMinions = getAllFromDatabase('minions');
+    //async check
+    if (allMinions) {
+        res.send(allMinions);
+    } else {
+        console.log(`something went wrong`);
+    }
+});
+
+minionsRouter.post('./minions', (req, res, next) => {
+    const newMinion = []
+    addToDatabase(req.queary)
+}
+)
+
+module.exports = minionsRouter;
--- server/api.js
+++ server/api.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const minionsRouter = require('./apiMinions');
 const apiRouter = express.Router();
+
+//post get update delete poss bodyparse, morgan
+
+
+apiRouter.use('/minions', minionsRouter);
 
 
 
 module.exports = apiRouter;
+
+// apiMinons.js
